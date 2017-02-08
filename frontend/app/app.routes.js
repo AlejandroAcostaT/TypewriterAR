62429b667b99f770bc5eaaae1c075b40cb2bca19
Alejandro Acosta <AlejandroAcostaT@users.noreply.github.com>
2017-02-08 18:01:36 -0400

    Routing added
    controllers, views, routes, and modules are now in different files.

--- frontend/app/app.routes.js
+++ frontend/app/app.routes.js
@@ -1,27 +1,17 @@
-(function(){
-	var app = angular.module('booksAR-routes', [])
-	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-		// any unknown URLS go to 404
-		$urlRouterProvider.otherwise('/404');
-		// no route goes to index
-		$urlRouterProvider.when('', '/');
-		// use a state provider for routing
+angular.module('routerApp')
 
-		$stateProvider
-		    .state('home', {
-		        url: '/',
-		        templateUrl: 'app/components/home/homeView.html',
-		    });
-		    /*.state('404', {
-		        url: '/404',
-		        templateUrl: 'app/shared/404.html'
-		    })
-		    .state('about', {
-		        // we'll add another state soon
-		        url: '/about',
-		        templateUrl: 'app/components/about/views/about.view.html',
-		    });
-	}]);
-
-	
-})();
+.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: './app/components/home/homeView.html',
+            controller: 'homeController',
+            controllerAs: 'homeCtrl'
+        });
+        
+});
